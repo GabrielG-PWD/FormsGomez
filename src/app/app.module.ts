a1b13d229bf5837de6d74616685934c7a1097d4c
GabrielG-PWD <85773507+GabrielG-PWD@users.noreply.github.com>
2023-07-12 00:37:02 -0300

    arreglando errores de ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, UsuarioComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule, CommonModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
