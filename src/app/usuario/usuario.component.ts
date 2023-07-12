fd961b9b61a37da725631d86717da605d0565120
GabrielG-PWD <85773507+GabrielG-PWD@users.noreply.github.com>
2023-07-12 01:31:45 -0300

    agregando validacion

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class UsuarioComponent {
   constructor() {}
-  registrado = false
-  mensaje=''
+  registrado = false;
+  mensaje = '';
 
   userModel = {
     email: '',
@@ -18,7 +18,14 @@ export class UsuarioComponent {
   };
 
   onSubmit(): void {
-    this.registrado = true
-    this.mensaje="usuario registrado con éxito"
+    if (
+        this.userModel.name.length != 0 &&
+        this.userModel.email.length != 0  &&
+        this.userModel.password.length != 0  &&
+        this.userModel.name.length != 0
+    ) {
+      this.registrado = true;
+      this.mensaje = 'usuario registrado con éxito';
+    }
   }
 }
